8f3b4ac117edd08988f8a181b502401d314b82c1
VinDau <44034064+VinDau@users.noreply.github.com>
2018-11-13 01:59:02 -0600

    Add files via upload

--- Gameplay.java
+++ Gameplay.java
@@ -1,11 +1,13 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.Timer;
+
 
 import javax.swing.JPanel;
 
@@ -22,8 +24,11 @@ private int ballPosX = 120;
 private int ballPosY = 350;
 private int ballXdir = -1;
 private int ballYdir = -2;
+
+private MapGenerator map;
 	
 public Gameplay() {
+	map = new MapGenerator(3, 7);
 	addKeyListener(this);
 	setFocusable(true);
 	setFocusTraversalKeysEnabled(false);
@@ -35,6 +40,10 @@ public void paint(Graphics g) {
 	//background
 	g.setColor(Color.black);
 	g.fillRect(1, 1, 692, 592);
+	
+	//Create map
+	map.draw((Graphics2D)g);
+	
 	
 	//borders
 	g.setColor(Color.black);
@@ -67,6 +76,37 @@ public void paint(Graphics g) {
 				ballYdir = -ballYdir;
 			}
 			
+			A: for(int i =0; i < map.map.length; i++) {
+				for(int j = 0; j < map.map[0].length; j++) {
+					if(map.map[i][j] > 0) {
+						int brickX = j * map.brickWidth + 80;
+						int brickY = i * map.brickHeight + 50;
+						int brickWidth = map.brickWidth;
+						int brickHeight = map.brickHeight;
+						
+						Rectangle rect = new Rectangle(brickX, brickY, brickWidth, brickHeight);
+						Rectangle ballRect = new Rectangle(ballPosX, ballPosY, 20, 20);
+						Rectangle brickRect = rect;
+						
+						if(ballRect.intersects(brickRect)) {
+							map.setBrickValue(0, i,  j);
+							totalBricks--;
+							score =+ 5;
+							
+							if(ballPosX + 19 <= brickRect.x || ballPosX + 1 >= brickRect.x + brickRect.width) {
+								ballXdir = -ballXdir;
+							} else {
+								ballYdir = -ballYdir;
+							}
+							
+							break A;
+							
+						}
+					
+					}
+				}
+			}
+			
 			ballPosX += ballXdir;
 			ballPosY += ballYdir;
 			if(ballPosX < 0) {
@@ -80,6 +120,8 @@ public void paint(Graphics g) {
 			if(ballPosX > 670) {
 				ballXdir = -ballXdir;
 			}
+			
+			
 		}
 		
 		repaint();		
