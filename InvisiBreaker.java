0cc1dfbbf7533070b565a30d16075dc7a421aa1b
VinDau <44034064+VinDau@users.noreply.github.com>
2018-11-14 22:42:33 -0600

    Add files via upload

--- InvisiBreaker.java
+++ InvisiBreaker.java
@@ -8,7 +8,7 @@ public class InvisiBreaker {
 		JFrame obj = new JFrame();
 		Gameplay gamePlay = new  Gameplay();
 		obj.setBounds(10, 10, 700, 600);
-		obj.setTitle("Brick Breaker");
+		obj.setTitle("Invisi-Breaker");
 		obj.setResizable(false);
 		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		obj.add(gamePlay);
